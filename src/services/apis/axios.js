bb4a5fa9dcbad7599ffb7de9debfd7a0a725a932
shanmukh <shanmukh0908@gmail.com>
2026-03-16 21:57:41 +0530

    commit:15

--- src/services/apis/axios.js
+++ src/services/apis/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
-  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
+  // baseURL: 'http://localhost:3000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
   withCredentials: true,
 });
 
